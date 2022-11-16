7d06257386b454caf3801888a3063337f653243b
LaNs992 <106981716+LaNs992@users.noreply.github.com>
2022-11-17 01:19:58 +0300

    Добавьте файлы проекта.

---
+++ SCLlib/bisnesSclad.cs
@@ -0,0 +1,30 @@
+﻿using склад;
+namespace SCLlib
+{
+    public class bisnesSclad
+    {
+
+        private List<sclad> ScldBL = new List<sclad>();
+
+        public bisnesSclad() { }
+        public List<sclad> Get()
+        {
+            return ScldBL;
+        }
+
+        public void Add(sclad data)
+        {
+            ScldBL.Add(data);
+        }
+        public void Remove(sclad data)
+        {
+            ScldBL.Remove(data);
+        }
+
+        public void Update(sclad id, sclad data)
+        {
+            var index = ScldBL.IndexOf(id);
+            ScldBL[index] = data;
+        }
+    }
+}
---
+++ ScladLogic/UnitTest1.cs
@@ -0,0 +1,79 @@
+using SCLlib;
+using склад;
+
+namespace ScladLogic
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void AddButton()
+        {
+            bisnesSclad SCL = new();
+            var result = SCL.Get();
+            sclad mytest = new()
+            {
+               name = "Франция",
+                raz = "13",
+                mater = "железо",
+                kol = 100,
+                min = 2,
+                price = 234,
+                fulprice = 280.4 ,
+           
+            };
+            SCL.Add(mytest);
+            Assert.Equal(mytest, result[0]);
+        }
+
+        [Fact]
+        public void ChangeButton()
+        {
+            bisnesSclad SCL = new();
+            var result = SCL.Get();
+            sclad mytest = new()
+            {
+                name = "стол",
+                raz = "13",
+                mater = "железо",
+                kol = 100,
+                min = 2,
+                price = 234,
+                fulprice = 280.4,
+            };
+            SCL.Add(mytest);
+            sclad mytest2 = new()
+            {
+                name = "Коробка",
+                raz = "13",
+                mater = "железо",
+                kol = 70,
+                min = 2,
+                price = 234,
+                fulprice = 280.4,
+            };
+            SCL.Update(mytest,mytest2);
+            Assert.Equal(mytest2, result[0]);
+        }
+
+        [Fact]
+        public void DeleteButton()
+        {
+            bisnesSclad SCL = new();
+            var result = SCL.Get();
+            sclad mytest = new()
+            {
+
+                name = "стол",
+                raz = "13",
+                mater = "железо",
+                kol = 100,
+                min = 2,
+                price = 234,
+                fulprice = 280.4,
+            };
+            SCL.Add(mytest);
+            SCL.Remove(mytest);
+            Assert.Empty(result);
+        }
+    }
+}
